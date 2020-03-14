a4530d5502a174031598ba94adad0f73a2949aba
Saurabh Saxena <saurabh10487@gmail.com>
2020-03-14 16:08:22 +0530

    Dissolve Shader
    dissolve effect shader & script

---
+++ ShaderGraph/Dissolve/DissolveEffect.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveEffect : MonoBehaviour
+{
+    Material material;
+
+    bool isDissolving = false;
+    float fade = 0f;
+
+    void Start()
+    {
+        // Get a reference to the material
+        material = GetComponent<SpriteRenderer>().material;
+
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Semicolon))
+        {
+            isDissolving = true;
+        }
+
+        if (isDissolving)
+        {
+            fade -= Time.deltaTime;
+
+            if (fade <= 0f)
+            {
+                fade = 0f;
+
+                isDissolving = false;
+            }
+            Debug.Log("fade: " + fade);
+
+            // Set the property
+
+        }
+        else
+        {
+
+
+
+
+            if (fade <= 1f)
+            {
+
+                fade += Time.deltaTime;
+
+            }
+        }
+        material.SetFloat("_Fade", fade);
+    }
+}
